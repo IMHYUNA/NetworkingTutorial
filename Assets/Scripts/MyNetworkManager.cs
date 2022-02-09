8dc1b0532a73a42b548b4a4cfc9efc720ce9f37e
IMHYUNA <37675537+IMHYUNA@users.noreply.github.com>
2022-02-09 21:31:45 +0900

    Hello world - multipkayer edition

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,27 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyNetworkManager : NetworkManager //callback에 대한 권한 액세스하는 단어
+{
+    public override void OnStartServer()
+    {
+        Debug.Log("Server Started!");
+    }
+
+    public override void OnStopServer()
+    {
+        Debug.Log("Server Stopped!");
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        Debug.Log("Connected to Server!");
+    }
+
+    public override void OnClientDisconnect(NetworkConnection conn)
+    {
+        Debug.Log("Disconnected to Server!");
+    }
+}
